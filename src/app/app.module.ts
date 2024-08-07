3b03be2e06a8f3cdc27585d4ebb43093eb9578b5
Denis Parra <denis.parra.89@outlook.com>
2024-08-07 18:38:17 -0400

    Added main component

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { SearchListComponent } from './search-list/search-list.component';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { YouTubePlayer, YOUTUBE_PLAYER_CONFIG } from '@angular/youtube-player';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchListComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    BrowserModule,
+    AppRoutingModule,
+    MatGridListModule,
+    MatIconModule,
+    MatFormFieldModule,
+    YouTubePlayer,
+    MatInputModule
+  ],
+  providers: [
+    provideAnimationsAsync(),
+    {
+      provide: YOUTUBE_PLAYER_CONFIG,
+      useValue: {
+        loadApi: false
+      }
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
